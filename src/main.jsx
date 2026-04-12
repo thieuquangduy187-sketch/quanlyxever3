8c8a1a05a89d11a5791828b0dc6ef628cf803db7
thieuquangduy187-sketch <thieuquangduy187@gmail.com>
2026-04-12 20:57:17 +0700

    Add client-side routing to main.jsx
    Implement client-side routing to load components dynamically based on the current path.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// Simple client-side routing
+const path = window.location.pathname
+
+async function mount() {
+  let Component
+
+  if (path === '/xe-detail') {
+    const mod = await import('./pages/XeDetail')
+    Component = mod.default
+  } else {
+    const mod = await import('./App')
+    Component = mod.default
+  }
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>
+  )
+}
+
+mount()
